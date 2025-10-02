6e5250c3848ba6bd435f107ac1d9baab96099bce
Sai Mahadev <mahadevvone@gmail.com>
2025-10-02 18:46:19 +0530

    feat: [LANGCHAIN][REACT]Replace Pydantic output parser with with structured output

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+# from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
@@ -17,13 +17,18 @@ from schemas import AgentResponse
 tools = [TavilySearch()]
 
 llm = ChatOpenAI(model="gpt-4")
+
+structured_llm = llm.with_structured_output(AgentResponse)
+
 react_prompt = hub.pull("hwchase17/react")
 
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+# output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 agent = create_react_agent(
     llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
@@ -32,9 +37,9 @@ agent = create_react_agent(
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 extract_ouptut = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+# parse_output = RunnableLambda(lambda x: output_parser.parse(x))
 
-chain = agent_executor | extract_ouptut | parse_output
+chain = agent_executor | extract_ouptut | structured_llm  # | parse_output
 
 
 def main():
