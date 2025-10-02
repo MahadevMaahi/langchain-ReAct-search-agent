25f069335a0de9d9f2727815e935471910d2c299
Sai Mahadev <mahadevvone@gmail.com>
2025-10-02 17:59:22 +0530

    feat: [LANGCHAIN][REACT]Custom Prompt to React Agent

--- main.py
+++ main.py
@@ -5,35 +5,42 @@ load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 
 llm = ChatOpenAI(model="gpt-4")
 react_prompt = hub.pull("hwchase17/react")
 
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
 agent = create_react_agent(
-    llm=llm,
-    tools=tools,
-    prompt=react_prompt
+    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
 )
 
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True
-)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 chain = agent_executor
+
 
 def main():
     print("Hello from langchain-react-search-agent!")
     result = chain.invoke(
         input={
             "input": "search for 3 job posting for AI engineer using langchain in bangalore on linkedin and list their detials",
-        
         }
     )
+    print(result)
 
 
 if __name__ == "__main__":
